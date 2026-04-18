68e00fb3074ee1b7b9059208d94800e0376ab1fc
medfordengineering <schristy@medford.k12.ma.us>
2026-04-18 17:22:38 -0400

    two pulse version working

--- cameratester.py
+++ cameratester.py
@@ -1,4 +1,5 @@
-from time import sleep
+#from time import sleep_ms
+import time
 from pulse_measure import PulseWidth
 from machine import Pin, SPI, PWM
 #from enum import Enum
@@ -9,6 +10,7 @@ EV_SOURCE = 1
 MENU = 2
 LINE = 10
 
+LED_PIN = 20
 
 
 def menu_screen():
@@ -37,11 +39,12 @@ BH = 30
 sensor1=PulseWidth(13)
 sensor2=PulseWidth(12)
 
+light = Pin(LED_PIN, Pin.OUT)
 # Set up PWM Pin
-panel_pin = machine.Pin(20)
-pwm = PWM(panel_pin)
-frequency = 5000
-pwm.freq(frequency)
+#panel_pin = machine.Pin(LED_PIN)
+#pwm = PWM(panel_pin)
+#frequency = 5000
+#pwm.freq(frequency)
 
 btn_up = Pin(21, Pin.IN, Pin.PULL_UP)
 btn_dn = Pin(22, Pin.IN, Pin.PULL_UP)
@@ -55,27 +58,42 @@ global state
 state = MENU
 menu_screen()
 
+pulse1 = False
+pulse2 = False
 
 while True:
+    #time.sleep_ms(100)
     if state == SHUTTER_SPEED:
             
         #SET LED PANEL TO FULL ON--NO PWM
-
-        #THE FOLLOWING LINE IS BLOCKING
-        period = sensor1.pulse_width()
         
-        speed = period /1000000
-        speed = 1/speed
+        if pulse1 == False:
+            period1 = sensor1.pulse_width()
+            if period1 != 0:
+                speed1 = period1 /1000000
+                speed1 = 1/speed1
+                pulse1 = True
+            
+        if pulse2 == False:
+            period2 = sensor2.pulse_width()
+            if period2 != 0:
+                speed2 = period2 /1000000
+                speed2 = 1/speed2
+                pulse2 = True
         
-        display.fill_rect(0, LINE, 100, LINE*3, 0)
-        display.text("TI: " + str(period) +"us",0,LINE,1)
-        display.text("SS: 1/" + str(int(speed)), 0,LINE*2,1) #ROUND SPEED
-        display.text("1) Restart", 0,LINE*3,1)
-        display.show()
-        state = MENU
-        #display.fill_rect(0, 20, 120, 8, 0)  
-        #display.text("Press Next",0,20,1)
-        #display.show()
+        if (pulse1 == True) and (pulse2 == True):
+            display.fill_rect(0, LINE, 100, LINE*3, 0)
+            display.text("TI: " + str(period1) +"us",0,LINE,1)
+            display.text("SS: 1/" + str(int(speed1)), 0,LINE*2,1) #ROUND SPEED
+            display.text("T2: " + str(period2) +"us",0,LINE*3,1)
+            display.text("S2: 1/" + str(int(speed2)), 0,LINE*4,1) #ROUND SPEED
+            display.text("1) Restart", 0,LINE*5,1)
+            display.show()
+            state = MENU
+      #  else:
+       #     counter = counter + 1
+         #   print(counter)
+        
             
     elif state == EV_SOURCE:
         pwm.duty_u16(dimming_level)
@@ -99,8 +117,12 @@ while True:
             display.show()
                     
     elif state == MENU:
+        #print("ready")
         if btn_up.value() == 0:
             state = SHUTTER_SPEED
+            #Turn on light panel
+            
+            light.value(1)
             #display.fill_rect(0, 20, 120, 8, 0)
             display.fill(0)
             display.text("SHUTTER SPEED",15,0,1)
