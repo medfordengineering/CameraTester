4de79f57fdfac7d084ca335a5da8cfb54fe716ae
medfordengineering <schristy@medford.k12.ma.us>
2026-04-22 11:25:20 -0400

    Working with three sensors

--- cameratester.py
+++ cameratester.py
@@ -15,8 +15,10 @@ LED_PIN = 20
 BTN_ONE = 21
 BTN_TWO = 22
 BTN_TRE = 18
+
 TOP_SENSOR = 13
-BOTTOM_SENSOR = 12
+MID_SENSOR = 12
+BTM_SENSOR = 11
 
 def menu_screen():
     display.fill(0)
@@ -36,10 +38,12 @@ display.rotate(True)
 display.fill(0)                         # Clear screen (0=black, 1=white)
 display.show()
 
-bottom_sensor=PulseWidth(BOTTOM_SENSOR)
+top_sensor=PulseWidth(TOP_SENSOR)
+mid_sensor=PulseWidth(MID_SENSOR)
+btm_sensor=PulseWidth(BTM_SENSOR)
 
-front_curtain=FirstCurtain(4,BOTTOM_SENSOR,TOP_SENSOR)
-rear_curtain=SecondCurtain(3,BOTTOM_SENSOR, TOP_SENSOR)
+front_curtain=FirstCurtain(4,BTM_SENSOR,TOP_SENSOR)
+rear_curtain=SecondCurtain(3,BTM_SENSOR, TOP_SENSOR)
 
 light = Pin(LED_PIN, Pin.OUT)
 # Set up PWM Pin
@@ -59,46 +63,65 @@ state = MENU
 
 menu_screen()
 
-pulse1 = False
-pulse2 = False
-pulse3 = False
-pulse4 = False
+ct_1st = False
+ct_2nd = False
+
+ss_top = False
+ss_mid = False
+ss_btm = False
 
 while True:
 
     if state == SHUTTER_SPEED:
             
         #SET LED PANEL TO FULL ON--NO PWM
-        if pulse3 == False:
+        if ct_1st == False:
             period = front_curtain.curtain_speed()
             if period != 0:
-                #front_curtain_travel = 24* period/16
-                front_curtain_travel = period
-                pulse3 = True
+                front_curtain_travel = 22.5* period/16
+                #front_curtain_travel = period
+                ct_1st = True
         
-        if pulse4 == False:
+        if ct_2nd == False:
             period = rear_curtain.curtain_speed()
             if period != 0:
-                #rear_curtain_travel = 24* period/16
-                rear_curtain_travel = period
-                pulse4 = True
+                rear_curtain_travel = 25.5* period/16
+                #rear_curtain_travel = period
+                ct_2nd = True
                 
-        if pulse2 == False:
-            period = bottom_sensor.pulse_width()
+        if ss_top == False:
+            period = top_sensor.pulse_width()
             if period != 0:
-                speed_top = 1000000/period
-                pulse2 = True
+                speed_top = 1000000/(period-200)
+                ss_top = True
+                
+        if ss_mid == False:
+            period = mid_sensor.pulse_width()
+            if period != 0:
+                speed_mid = 1000000/(period-200)
+                ss_mid = True
+                
+        if ss_btm == False:
+            period = btm_sensor.pulse_width()
+            if period != 0:
+                speed_btm = 1000000/(period-200)
+                ss_btm = True
 
-        if (pulse3 == True) and (pulse4 == True) and (pulse2 == True):
+        if (ct_2nd == True) and (ct_1st == True) and (ss_btm == True) and (ss_mid == True) and (ss_top == True):
                 display.fill_rect(0, LINE, 100, LINE*3, 0)
                 display.text("FC: " + str(front_curtain_travel) +"us",0,LINE,1)
                 display.text("RC: " + str(rear_curtain_travel) +"us",0,LINE*2,1)
-                display.text("SS1: 1/" + str(int(speed_top)), 0,LINE*3,1) #ROUND SPEED
-                display.text("1) Restart", 0,LINE*5,1)
+                display.text("SST: 1/" + str(int(speed_top)), 0,LINE*3,1) #ROUND SPEED
+                display.text("SSM: 1/" + str(int(speed_mid)), 0,LINE*4,1) #ROUND SPEED
+                display.text("SSB: 1/" + str(int(speed_btm)), 0,LINE*5,1) #ROUND SPEED
+
+                display.text("1) Restart", 0,LINE*6,1)
                 display.show()
-                pulse2 = False
-                pulse3 = False
-                pulse4 = False
+                ct_1st = False
+                ct_2nd = False
+                ss_btm = False
+                ss_mid = False
+                ss_top = False
                 state = MENU
             
     elif state == EV_SOURCE:
